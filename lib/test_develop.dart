ab896ffe3a92481642f17b6c7f7c5d9a1e54f1ab
alessandrodagostino <alessandrodagostino@andemili.com>
2019-12-05 15:00:34 +0100

    internetCheck

--- lib/test_develop.dart
+++ lib/test_develop.dart
@@ -1,7 +1,28 @@
 library test_develop;
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+class Utility {
+  
+  internetCheck() async {
+    final response = await http.get('https://google.it');
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      print("No Internert");
+      return false;
+    }
+  }
+
+  customAppBar(){
+   
+  }
+  
 }
